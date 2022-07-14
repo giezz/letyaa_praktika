9bd839b699de6cb574fa742b590de954e571019b
giezz <90324706+giezz@users.noreply.github.com>
2022-07-14 10:38:08 +0300

    some updates

--- random_uniform/run.py
+++ random_uniform/run.py
@@ -3,8 +3,8 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-m = 32768
-# m = 10
+# m = 32768
+m = 1000
 a = 23
 c = 12345
 
@@ -20,7 +20,7 @@ def lcm(seed, size):
     return r[1:size]
 
 
-data = np.array(lcm(time.time(), 100))
+data = np.array(lcm(time.time(), 10))
 data = np.sort(data)
 print(data)
 
@@ -29,26 +29,24 @@ F_x = []
 for i in range(data.size - 1):
     F_x.append((data[i] - min(data)) / (max(data) - min(data)))
 
-
 F_x = np.sort(F_x)
+# print(F_x)
+
+plt.hist(data, bins=30, density=True, alpha=0.6,
+         label='Гистограмма случайной величины')
+plt.axhline(f_x, color='red', label='Плотность случайной величины')
+plt.legend(fontsize=14, loc=1)
+plt.grid(ls=':')
+plt.show()
+plt.close()
+plt.plot(F_x, label='Функция распределения')
+plt.show()
 
 # оценка максимального правдободобия
 R_A = data
 theta_A = R_A / np.sum(R_A)
 print(f'оценка максимального прадободобия {theta_A}')
 
-plt.hist(data, bins=30, density=True, alpha=0.6, label='Гистограмма случайной величины', edgecolor='black')
-plt.hlines(f_x, xmin=min(data), xmax=max(data), color='red', label='Плотность случайной величины')
-plt.legend(fontsize=14, loc=3)
-plt.grid(ls=':')
-plt.show()
-plt.close()
-plt.plot(F_x, label='Функция распределения')
-plt.legend(fontsize=14, loc=1)
-plt.show()
-plt.close()
-plt.plot(theta_A, label='Оценка максимального правдободобия')
-plt.legend(fontsize=14, loc=1)
-plt.show()
+
 
 
